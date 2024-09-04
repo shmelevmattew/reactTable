0bb29ac06493c819e2fa26945bd96189192b2fbd
@SHMELEV183_EXT <shmelev.mv@mmk.ru>
2024-09-04 11:41:49 +0500

    tableDone

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import supabase from "./api/client";
+import { getTableData, getTableDataByName } from "./api/service/tableService";
+import { Table } from "@mui/material";
+import BasicTable from "./components/Table/Table";
+import Header from "./components/Header/Header";
+
+function App() {
+  const [rows,setRows] = useState([])
+  useEffect(()=>{
+    console.log(supabase)
+    console.log(getTableData().then(res=>setRows(res)))
+  },[])
+  return (
+    <div className="App">
+      <Header setRows={setRows}/>
+      <BasicTable rows={rows}></BasicTable> 
+    </div>
+  );
+}
+
+export default App;
